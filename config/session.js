9a4f04c8f0d4ba2ad470306536800ec25e739f54
Veeti K <veetik@protonmail.com>
2021-08-01 18:35:52 +0300

    new env variable

--- config/session.js
+++ config/session.js
@@ -7,7 +7,10 @@ export const {
   SESSION_NAME,
   SESSION_ABSOLUTE_TIMEOUT = 1000 * 60 * 58, // 58 min, cause google token expires in 1 hour
   SESSION_IDLE_TIMEOUT = SESSION_ABSOLUTE_TIMEOUT,
+  PROD = 0,
 } = process.env;
+
+const prod = PROD == 1;
 
 export const SESSION_OPTIONS = {
   secret: SESSION_SECRET,
@@ -16,11 +19,12 @@ export const SESSION_OPTIONS = {
   rolling: true,
   resave: false,
   saveUninitialized: false,
-  httpOnly: true, // --
+  httpOnly: prod,
   unset: "destroy",
-  proxy: true, // --
+  proxy: prod,
   cookie: {
     maxAge: +SESSION_ABSOLUTE_TIMEOUT,
-    secure: true, // --
+    secure: prod,
+    sameSite: "lax",
   },
 };
