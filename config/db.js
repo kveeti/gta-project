fc9afebbead31f5e2170d20d29e2ba948f4b38ad
Veeti K <veetik@protonmail.com>
2021-08-16 02:00:09 +0300

    uses mongo uri if defined

--- config/db.js
+++ config/db.js
@@ -1,12 +1,20 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
 dotenv.config();
 
-export const MONGO_URI = `mongodb://${process.env.MONGO_USERNAME}:${encodeURIComponent(process.env.MONGO_PASSWORD)}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}?authSource=${process.env.MONGO_AUTHSOURCE}`
+export let MONGO_URI = `mongodb://${
+  process.env.MONGO_USERNAME
+}:${encodeURIComponent(process.env.MONGO_PASSWORD)}@${process.env.MONGO_HOST}:${
+  process.env.MONGO_PORT
+}/${process.env.MONGO_DATABASE}?authSource=${process.env.MONGO_AUTHSOURCE}`;
+
+if (process.env.MONGO_URI) {
+  MONGO_URI = process.env.MONGO_URI;
+}
 
 export const MONGO_OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
-}
+  useFindAndModify: false,
+};
