6955af3a8faa472ff567df4c7f549ff0ca2cb96c
Veeti K <veetik@protonmail.com>
2021-12-13 17:38:45 +0200

    move style import

--- v2/front/pages/_app.tsx
+++ v2/front/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "../state/store";
 
 import "../styles/globals.css";
+import "semantic-ui-css/semantic.min.css";
 
 export default ({ Component, pageProps: { session, ...pageProps } }) => (
   <Provider store={store}>
--- v2/front/pages/index.tsx
+++ v2/front/pages/index.tsx
@@ -1,7 +1,5 @@
 import { MenuBar } from "../components/MenuBar/MenuBar";
 
-import "semantic-ui-css/semantic.min.css";
-
 export default () => {
   return <MenuBar />;
 };
