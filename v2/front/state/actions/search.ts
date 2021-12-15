bc2ca4c02a0409affb129b11c33e47e7c37642fa
Veeti K <veetik@protonmail.com>
2021-12-15 19:28:04 +0200

    hmm

--- v2/front/state/actions/search.ts
+++ v2/front/state/actions/search.ts
@@ -64,7 +64,7 @@ export const search = (query: string) => async (dispatch: any) => {
 
     dispatch(api.setLoading(true));
 
-    const res = await axios(getNextAxiosConfig("/search", "GET", query));
+    const res = await axios(getNextAxiosConfig(`/search?q=${query}`, "GET"));
 
     dispatch(set.cars(res.data.cars));
     dispatch(set.garages(res.data.garages));
