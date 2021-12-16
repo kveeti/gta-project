643b1b8ffb4063afe175d152328c5288fc234bfa
Veeti K <veetik@protonmail.com>
2021-12-17 00:19:58 +0200

    new garage button goes to new garage page

--- v2/front/components/MenuBar/Buttons/NewGarage.tsx
+++ v2/front/components/MenuBar/Buttons/NewGarage.tsx
@@ -1,11 +1,20 @@
 import { PlusIcon } from "@radix-ui/react-icons";
+import { useRouter } from "next/router";
 import { MenubarBtn } from "./Styles";
 
-const NewGarageButton = () => (
-  <MenubarBtn white>
-    <PlusIcon />
-    <img src="/icons/garage-icon.png" alt="car icon" height={23} />
-  </MenubarBtn>
-);
+const NewGarageButton = () => {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push("/new/garage");
+  };
+
+  return (
+    <MenubarBtn white onClick={() => onClick()}>
+      <PlusIcon />
+      <img src="/icons/garage-icon.png" alt="car icon" height={23} />
+    </MenubarBtn>
+  );
+};
 
 export default NewGarageButton;
