feaaca01f2a7cebcc44e70f0b7fa9d3a6c099cff
Veeti K <veetik@protonmail.com>
2021-12-17 00:50:55 +0200

    added new garage actions to index so actions can be imported

--- v2/front/state/actions/index.ts
+++ v2/front/state/actions/index.ts
@@ -2,10 +2,12 @@ import * as users from "./users";
 import * as search from "./search";
 import * as check from "./check";
 import * as newCar from "./newCar";
+import * as newGarage from "./newGarage";
 
 export const actions = {
   users,
   search,
   check,
   newCar,
+  newGarage,
 };
