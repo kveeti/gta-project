9963c6b3eb8f8ad89a57497ac31954b3eb5882d8
Veeti K <veetik@protonmail.com>
2021-12-18 00:18:46 +0200

    so heres the resize listener i was talking about

--- v2/front/components/Layout.tsx
+++ v2/front/components/Layout.tsx
@@ -1,10 +1,34 @@
 import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { actions } from "../state/actions";
+import { useISelector } from "../state/hooks";
 import { SignInCard } from "./Cards/Signin/Signin";
 import { Content, Main, Section } from "./Containers/Containers";
 import { MenuBar } from "./MenuBar/MenuBar";
 import { Sidebar } from "./Sidebar/Sidebar";
 
 const Layout = ({ children }) => {
+  if (typeof window === "undefined") return null;
+
+  const dispatch = useDispatch();
+  const state = useISelector((state) => state);
+  const mobile = state.bp === 1;
+
+  useEffect(() => {
+    const handleResize = () => {
+      let bp = 3;
+
+      if (window.innerWidth < 900) bp = 1;
+
+      if (state.bp !== bp) dispatch(actions.bp.set(bp));
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  });
+
   const { data, status } = useSession();
 
   if (status === "loading") return null;
@@ -16,7 +40,7 @@ const Layout = ({ children }) => {
       <MenuBar />
       <Content>
         <Main center>{children}</Main>
-        <Sidebar />
+        {!mobile && <Sidebar />}
       </Content>
     </Section>
   );
