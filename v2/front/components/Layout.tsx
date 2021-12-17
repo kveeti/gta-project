ea4a3abddc188728fa931a211ee84632e94537b6
Veeti K <veetik@protonmail.com>
2021-12-17 12:45:35 +0200

    signin page, and redirection if no session

--- v2/front/components/Layout.tsx
+++ v2/front/components/Layout.tsx
@@ -1,15 +1,25 @@
+import { useSession } from "next-auth/react";
+import { SignInCard } from "./Cards/Signin/Signin";
 import { Content, Main, Section } from "./Containers/Containers";
 import { MenuBar } from "./MenuBar/MenuBar";
 import { Sidebar } from "./Sidebar/Sidebar";
 
-const Layout = ({ children }) => (
-  <Section>
-    <MenuBar />
-    <Content>
-      <Main center>{children}</Main>
-      <Sidebar />
-    </Content>
-  </Section>
-);
+const Layout = ({ children }) => {
+  const { data, status } = useSession();
+
+  if (status === "loading") return null;
+
+  if (!data) return <SignInCard />;
+
+  return (
+    <Section>
+      <MenuBar />
+      <Content>
+        <Main center>{children}</Main>
+        <Sidebar />
+      </Content>
+    </Section>
+  );
+};
 
 export default Layout;
