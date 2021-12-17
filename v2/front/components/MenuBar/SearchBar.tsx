e9cf8bedce748053a8e95aa4cd3f1b3462ee42a1
Veeti K <veetik@protonmail.com>
2021-12-17 17:15:32 +0200

    using my own new input for the search

--- v2/front/components/MenuBar/SearchBar.tsx
+++ v2/front/components/MenuBar/SearchBar.tsx
@@ -1,14 +1,7 @@
 import { useRouter } from "next/router";
 import { useDispatch } from "react-redux";
 import { actions } from "../../state/actions";
-import { styled } from "../../stitches.config";
-import { BaseInput } from "../Styles/Inputs";
-
-const StyledInput = styled(BaseInput, {
-  width: "100%",
-  height: "2rem",
-  margin: "1rem 0.5rem 1rem 1rem",
-});
+import { Input } from "../Styles/Inputs";
 
 export const SearchBar = () => {
   if (typeof window === "undefined") return null;
@@ -17,6 +10,8 @@ export const SearchBar = () => {
   const router = useRouter();
 
   const onSearchTermChange = async (value: string) => {
+    dispatch(actions.search.set.input(value));
+
     if (!value) {
       dispatch(actions.search.api.reset());
       dispatch(actions.search.set.cars([]));
@@ -35,15 +30,14 @@ export const SearchBar = () => {
   };
 
   return (
-    <StyledInput
+    <Input
       white
-      id="garage-input"
-      type="text"
-      autoComplete="off"
       autoFocus
-      placeholder="Search for cars/garages..."
+      id="garage-input"
+      type="search"
+      placeholder="Search for garages/cars..."
       onChange={(e) => onSearchTermChange(e.target.value)}
-      value={router.query.q || ""}
+      value={router.query.q?.toString() || ""}
     />
   );
 };
