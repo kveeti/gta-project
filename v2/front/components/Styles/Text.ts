c1a36ac1281e3d95a0eaba4ec2688ed982ddf483
Veeti K <veetik@protonmail.com>
2021-12-17 19:53:19 +0200

    ill configure those later

--- v2/front/components/Styles/Text.ts
+++ v2/front/components/Styles/Text.ts
@@ -4,10 +4,6 @@ export const Text = styled("p", {
   fontSize: "1rem",
   margin: "0",
   padding: "0",
-
-  "media (min-width: 600px)": {
-    fontSize: "1rem",
-  },
 });
 
 export const Title = styled("h1", {
@@ -16,10 +12,6 @@ export const Title = styled("h1", {
   margin: "0",
   padding: "0",
   fontWeight: 500,
-
-  "media (min-width: 600px)": {
-    fontSize: "1rem",
-  },
 });
 
 export const Desc = styled("p", {
@@ -27,8 +19,4 @@ export const Desc = styled("p", {
   fontSize: "1rem",
   paddingTop: "1rem",
   paddingBottom: "2rem",
-
-  "media (min-width: 600px)": {
-    fontSize: "1rem",
-  },
 });
