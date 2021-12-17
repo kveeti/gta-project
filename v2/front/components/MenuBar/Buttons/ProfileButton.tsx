2f373cc4aaf4608aec53199cc589d11bf21e5d1d
Veeti K <veetik@protonmail.com>
2021-12-17 02:31:57 +0200

    profile button signs in

--- v2/front/components/MenuBar/Buttons/ProfileButton.tsx
+++ v2/front/components/MenuBar/Buttons/ProfileButton.tsx
@@ -1,9 +1,14 @@
 import { PersonIcon } from "@radix-ui/react-icons";
+import { signIn } from "next-auth/react";
 import { MenubarBtn } from "../Buttons/Styles";
 
 export const ProfileButton = () => {
+  const onClick = () => {
+    signIn();
+  };
+
   return (
-    <MenubarBtn transparent small>
+    <MenubarBtn transparent small onClick={() => onClick()}>
       <PersonIcon style={{ color: "white" }} transform="scale(1.6)" />
     </MenubarBtn>
   );
