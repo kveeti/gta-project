70f7fdd78a9315008377d87c1a20840ccf6c2935
Veeti K <veetik@protonmail.com>
2021-12-25 03:53:38 +0200

    nextjs api/cars endpoint supports delete method now

--- v2/front/pages/api/cars.tsx
+++ v2/front/pages/api/cars.tsx
@@ -4,7 +4,7 @@ import { getToken } from "next-auth/jwt";
 import { getApiAxiosConfigWithBody } from "../../state/actions/axiosConfig";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (!req.method || req.method !== "POST") return res.status(405).end();
+  if (!req.method) return res.status(405).end();
 
   const token = await getToken({
     req,
@@ -12,10 +12,30 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     raw: true,
   });
 
+  if (req.method === "POST") return POSThandler(req, res, token);
+  if (req.method === "DELETE") return DELETEhandler(req, res, token);
+};
+
+const POSThandler = async (req: NextApiRequest, res: NextApiResponse, token: string) => {
   try {
     if (!req.body) return res.status(400).end();
 
     const response = await axios(getApiAxiosConfigWithBody("/cars", "POST", token, req.body));
+
+    res.status(200).json(response.data);
+  } catch (err) {
+    console.log(err);
+
+    if (!err.response) return res.status(500).json({ error: "Server error" });
+    res.status(err.response.status).end();
+  }
+};
+
+const DELETEhandler = async (req: NextApiRequest, res: NextApiResponse, token: string) => {
+  try {
+    if (!req.body) return res.status(400).end();
+
+    const response = await axios(getApiAxiosConfigWithBody("/cars", "DELETE", token, req.body));
 
     res.status(200).json(response.data);
   } catch (err) {
