c85fa36a8c294cb06d2cd1ddf0977987724bad0a
Veeti K <veetik@protonmail.com>
2022-01-10 06:04:44 +0200

    error messageeeesss

--- v2/front/components/Sidebar/Buttons/DeleteBtn.tsx
+++ v2/front/components/Sidebar/Buttons/DeleteBtn.tsx
@@ -1,3 +1,4 @@
+import { toast } from "react-toastify";
 import { useISelector } from "../../../state/hooks";
 import { SidebarBtn } from "./Styles";
 
@@ -6,8 +7,14 @@ export const DeleteBtn = ({ onClick, open }) => {
 
   const showButton = checkedCars.length > 0;
 
+  const onBtnClick = (e) => {
+    if (!showButton) return toast.error("No cars selected");
+
+    onClick(e);
+  };
+
   return (
-    <SidebarBtn onClick={(e) => showButton && onClick(e)} red disabled={!showButton}>
+    <SidebarBtn onClick={(e) => onBtnClick(e)} red disabled={!showButton}>
       {open ? "Click again to confirm" : "Delete"}
     </SidebarBtn>
   );
--- v2/front/components/Sidebar/Buttons/MoveBtn.tsx
+++ v2/front/components/Sidebar/Buttons/MoveBtn.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 import { useISelector } from "../../../state/hooks";
 import { SidebarBtn } from "./Styles";
 
@@ -8,12 +9,14 @@ export const MoveBtn = () => {
 
   const showButton = checkedCars.length > 0;
 
+  const onClick = () => {
+    if (!checkedCars.length) return toast.error("No cars selected");
+
+    router.push("/move", "/move", { shallow: true });
+  };
+
   return (
-    <SidebarBtn
-      blue
-      disabled={!showButton}
-      onClick={() => router.push("/move", "/move", { shallow: true })}
-    >
+    <SidebarBtn blue disabled={!showButton} onClick={() => onClick()}>
       Move
     </SidebarBtn>
   );
