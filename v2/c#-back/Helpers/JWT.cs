86afc702c6a8f32463a5e404d2dc9b4dbb3ec0eb
Veeti K <veetik@protonmail.com>
2022-01-11 23:44:54 +0200

    jwt builder, also found online

---
+++ v2/c#-back/Helpers/JWT.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Backend.Helpers
+{
+    static class JWT
+    {
+        public static string BuildToken(string username, string role, string issuer, string audience, string key)
+        {
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.NameIdentifier, username),
+            new Claim(ClaimTypes.Role, role)
+            };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+            var tokenDescriptor = new JwtSecurityToken(issuer, audience, claims,
+                expires: DateTime.Now.AddMinutes(15), signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+    }
+}
