bc82d12cc033cd05454baae3ed2514cc665064d3
Veeti K <veetik@protonmail.com>
2022-01-11 23:12:29 +0200

    install efcore, add datacontext

---
+++ v2/c#-back/Data/IDataContext.cs
@@ -0,0 +1,11 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public interface IDataContext
+    {
+        DbSet<User> Users { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
