ce286a661371821eb380b5461bcff875eda8c982
Veeti K <veetik@protonmail.com>
2022-01-11 23:39:36 +0200

    user controller init

---
+++ v2/c#-back/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Backend.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepo _db;
+        private readonly IOptions<Settings> _settings;
+
+        public UserController(IUserRepo userRepo, IOptions<Settings> settings)
+        {
+            _db = userRepo;
+            _settings = settings;
+        }
+    }
+}
