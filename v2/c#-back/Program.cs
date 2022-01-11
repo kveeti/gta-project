fcf14b0d85cf70acf2a1e97637dc2fa0418de1c8
Veeti K <veetik@protonmail.com>
2022-01-11 22:46:03 +0200

    settings class and its injection

--- v2/c#-back/Program.cs
+++ v2/c#-back/Program.cs
@@ -1,9 +1,13 @@
+using Backend;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<Settings>(builder.Configuration.GetSection("Settings"));
 
 var app = builder.Build();
 
---
+++ v2/c#-back/Settings.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend
+{
+    public class Settings
+    {
+        public string JWT_Key { get; set; }
+        public string JWT_Issuer { get; set; }
+        public string JWT_Audience { get; set; }
+    }
+}
