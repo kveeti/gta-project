77dc37b5475cd487cab73c7ac23d7466002db470
Veeti K <veetik@protonmail.com>
2022-01-13 04:27:09 +0200

    UserController is fully tested for now

--- v2/c#-back/Backend.Tests/UserControllerTests.cs
+++ v2/c#-back/Backend.Tests/UserControllerTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Backend.Api;
 using Backend.Api.Controllers;
@@ -46,6 +47,101 @@ public class UserControllerTests
         var returnedUser = (result.Result as OkObjectResult).Value as ReturnUserDto;
         
         returnedUser.Should().BeEquivalentTo(expectedReturnUser);
+    }
+
+    [Fact]
+    public void GetAll_ReturnsAllUsers()
+    {
+        IEnumerable<ReturnUserDto> expectedUsers = new[]
+        {
+            FakeUserToDto(CreateFakeUser()),
+            FakeUserToDto(CreateFakeUser()),
+            FakeUserToDto(CreateFakeUser()),
+        };
+        
+        _fakeRepo.Setup(repo => repo.GetAll())
+            .Returns(expectedUsers);
+
+        var controller = new UserController(_fakeRepo.Object, _fakeSettings);
+
+        var result = controller.GetAll();
+
+        var returnedUsers = (result.Result as OkObjectResult).Value as IEnumerable<ReturnUserDto>;
+
+        returnedUsers.Should().BeEquivalentTo(expectedUsers);
+    }
+
+    [Fact]
+    public async Task UpdateRole_WithExistingUser_ReturnsUpdatedUser()
+    {
+        UpdateUserDto updateUserDto = new()
+        {
+            NewRole = "Admin"
+        };
+        var existingUser = CreateFakeUser();
+        var expectedUser = new ReturnUserDto()
+        {
+            Id = existingUser.Id,
+            Username = existingUser.Username,
+            Role = updateUserDto.NewRole
+        };
+        
+        _fakeRepo.Setup(repo => repo.GetById(It.IsAny<Guid>()))
+            .ReturnsAsync(existingUser);
+
+        var controller = new UserController(_fakeRepo.Object, _fakeSettings);
+
+        var result = await controller.UpdateRole(Guid.NewGuid(), updateUserDto);
+
+        var returnedUser = (result.Result as OkObjectResult).Value as ReturnUserDto;
+
+        returnedUser.Should().BeEquivalentTo(expectedUser);
+    }
+    
+    [Fact]
+    public async Task UpdateRole_WithNoExistingUser_ReturnsNotFound()
+    {
+        UpdateUserDto updateUserDto = new()
+        {
+            NewRole = "Admin"
+        };
+        
+        _fakeRepo.Setup(repo => repo.GetById(It.IsAny<Guid>()))
+            .ReturnsAsync((User)null);
+
+        var controller = new UserController(_fakeRepo.Object, _fakeSettings);
+
+        var result = await controller.UpdateRole(Guid.NewGuid(), updateUserDto);
+
+        result.Result.Should().BeOfType<NotFoundResult>();
+    }
+    
+    [Fact]
+    public async Task Delete_WithExistingUser_ReturnsNoContent()
+    {
+        var existingUser = CreateFakeUser();
+        
+        _fakeRepo.Setup(repo => repo.GetById(It.IsAny<Guid>()))
+            .ReturnsAsync(existingUser);
+
+        var controller = new UserController(_fakeRepo.Object, _fakeSettings);
+
+        var result = await controller.Delete(Guid.NewGuid());
+
+        result.Result.Should().BeOfType<NoContentResult>();
+    }
+    
+    [Fact]
+    public async Task Delete_WithNoExistingUser_ReturnsNotFound()
+    {
+        _fakeRepo.Setup(repo => repo.GetById(It.IsAny<Guid>()))
+            .ReturnsAsync((User)null);
+
+        var controller = new UserController(_fakeRepo.Object, _fakeSettings);
+
+        var result = await controller.Delete(Guid.NewGuid());
+
+        result.Result.Should().BeOfType<NotFoundResult>();
     }
 
     private static User CreateFakeUser()
