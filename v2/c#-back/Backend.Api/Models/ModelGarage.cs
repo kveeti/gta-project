70d78d09cf6071de8390aa5542d26cd4cd1f3422
Veeti K <veetik@protonmail.com>
2022-01-15 00:12:46 +0200

    new model and made the earlier models nicer looking

--- v2/c#-back/Backend.Api/Models/ModelGarage.cs
+++ v2/c#-back/Backend.Api/Models/ModelGarage.cs
@@ -2,13 +2,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Backend.Api.Models;
 
-public class ModelCar
+public class ModelGarage
 {
   [Key] public Guid Id { get; set; }
-
+  
   [Required] public string Name { get; set; }
-
-  [Required] public string Manufacturer { get; set; }
-
-  [Required] public string Class { get; set; }
+  
+  [Required] public int Capacity { get; set; }
+  
+  [Required] public string Type { get; set; }
 }
--- v2/c#-back/Backend.Api/Models/User.cs
+++ v2/c#-back/Backend.Api/Models/User.cs
@@ -4,12 +4,11 @@ namespace Backend.Api.Models;
 
 public class User
 {
-  [Key]
-  public Guid Id { get; set; }
-  [Required]
-  public string Username { get; set; }
-  [Required]
-  public string Password { get; set; }
-  [Required]
-  public string Role { get; set; }
+  [Key] public Guid Id { get; set; }
+  
+  [Required] public string Username { get; set; }
+  
+  [Required] public string Password { get; set; }
+  
+  [Required] public string Role { get; set; }
 }
