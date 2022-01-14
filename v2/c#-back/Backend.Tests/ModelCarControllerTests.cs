c57d813366c5a4557c82e3335f6aa5090c1d151c
Veeti K <veetik@protonmail.com>
2022-01-14 22:18:11 +0200

    tests updated for ModelCarController

--- v2/c#-back/Backend.Tests/ModelCarControllerTests.cs
+++ v2/c#-back/Backend.Tests/ModelCarControllerTests.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Backend.Api.Controllers;
 using Backend.Api.Dtos.ModelCarDtos;
 using Backend.Api.Models;
 using Backend.Api.Repositories;
 using FluentAssertions;
+using JetBrains.Annotations;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
@@ -62,14 +65,129 @@ public class ModelCarControllerTests
     (result.Result as OkObjectResult).Value.Should().BeEquivalentTo(newCar);
   }
   
-  private ModelCar CreateFakeModelCar()
+  [Fact]
+  public async Task Add_WithConflictingName_ReturnsConflictExistingCar()
+  {
+    ModelCarDto newCar = new()
+    {
+      Name = Guid.NewGuid().ToString(),
+      Manufacturer = Guid.NewGuid().ToString(),
+      Class = Guid.NewGuid().ToString()
+    };
+    var conflictingCar = CreateFakeModelCar();
+    
+    _fakeRepo.Setup(repo => repo.GetByName(It.IsAny<string>()))
+      .ReturnsAsync(conflictingCar);
+
+    var controller = new ModelCarController(_fakeRepo.Object);
+
+    var result = await controller.Add(newCar);
+
+    result.Result.Should().BeOfType<ConflictObjectResult>();
+    (result.Result as ConflictObjectResult).Value.Should().BeEquivalentTo(conflictingCar);
+  }
+
+  [Fact]
+  public async Task GetAll_WithNoQuery_ReturnsAllCars()
+  {
+    IEnumerable<ModelCar> expectedCars = new[]
+    {
+      CreateFakeModelCar(),
+      CreateFakeModelCar(),
+      CreateFakeModelCar()
+    };
+    
+    _fakeRepo.Setup(repo => repo.GetAll())
+      .ReturnsAsync(expectedCars);
+
+    var controller = new ModelCarController(_fakeRepo.Object);
+
+    var result = await controller.GetAll(null);
+
+    result.Result.Should().BeOfType<OkObjectResult>();
+    (result.Result as OkObjectResult).Value.Should().BeEquivalentTo(expectedCars);
+  }
+  
+  [Fact]
+  public async Task GetAll_WithQuery_ReturnsOnlyMatchingCars()
+  {
+    IEnumerable<ModelCar> allCars = new[]
+    {
+      CreateFakeModelCar("test-ok"),
+      CreateFakeModelCar("test-ok"),
+      CreateFakeModelCar("not-ok")
+    };
+
+    IEnumerable<ModelCar> expectedCars = allCars.Take(2);
+    
+    _fakeRepo.Setup(repo => repo.GetAll())
+      .ReturnsAsync(allCars);
+
+    var controller = new ModelCarController(_fakeRepo.Object);
+
+    var result = await controller.GetAll("test");
+
+    result.Result.Should().BeOfType<OkObjectResult>();
+    (result.Result as OkObjectResult).Value.Should().BeEquivalentTo(expectedCars);
+  }
+
+  [Fact]
+  public async Task Update_WithExistingCar_ReturnsUpdatedCar()
+  {
+    ModelCar existingCar = CreateFakeModelCar();
+    ModelCarDto dto = new()
+    {
+      Name = Guid.NewGuid().ToString(),
+      Manufacturer = Guid.NewGuid().ToString(),
+      Class = Guid.NewGuid().ToString()
+    };
+    ModelCar updatedCar = new()
+    {
+      Id = existingCar.Id,
+      Name = dto.Name,
+      Manufacturer = dto.Manufacturer,
+      Class = dto.Class
+    };
+    
+    _fakeRepo.Setup(repo => repo.GetById(It.IsAny<Guid>()))
+      .ReturnsAsync(existingCar);
+
+    var controller = new ModelCarController(_fakeRepo.Object);
+
+    var result = await controller.Update(existingCar.Id, dto);
+
+    result.Result.Should().BeOfType<OkObjectResult>();
+    (result.Result as OkObjectResult).Value.Should().BeEquivalentTo(updatedCar);
+  }
+
+  [Fact]
+  public async Task Update_WithNoExistingCar_ReturnsNotFound()
+  {
+    ModelCarDto dto = new()
+    {
+      Name = Guid.NewGuid().ToString(),
+      Manufacturer = Guid.NewGuid().ToString(),
+      Class = Guid.NewGuid().ToString()
+    };
+    
+    _fakeRepo.Setup(repo => repo.GetById(It.IsAny<Guid>()))
+      .ReturnsAsync((ModelCar)null);
+
+    var controller = new ModelCarController(_fakeRepo.Object);
+
+    var result = await controller.Update(Guid.NewGuid(), dto);
+
+    result.Result.Should().BeOfType<NotFoundResult>();
+  }
+  
+  private ModelCar CreateFakeModelCar(string? aAllProps = null)
   {
     return new()
     {
       Id = Guid.NewGuid(),
-      Name = Guid.NewGuid().ToString(),
-      Manufacturer = Guid.NewGuid().ToString(),
-      Class = Guid.NewGuid().ToString()
+      Name = aAllProps != null ? aAllProps : Guid.NewGuid().ToString(),
+      Manufacturer = aAllProps != null ? aAllProps : Guid.NewGuid().ToString(),
+      Class = aAllProps != null ? aAllProps : Guid.NewGuid().ToString()
     };
   }
 }
