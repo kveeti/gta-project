14980fe385ba4a10cc403e46661ebb7de7dba4bb
Veeti K <veetik@protonmail.com>
2022-01-22 22:21:59 +0200

    admin check for admin pages

---
+++ v2/front/hooks/useAdminCheck.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { actions } from "../state/actions";
+import { config } from "../util/axios";
+
+export const useAdminCheck = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  const [loading, setLoading] = useState(true);
+
+  const handleUnauthorized = () => {
+    router.push("/", "/", { shallow: true });
+  };
+
+  useEffect(() => {
+    const getMe = async () => {
+      const res = await axios(config("/users/me", "GET"));
+
+      if (res?.data) {
+        dispatch(actions.users.set.me(res?.data));
+
+        if (res?.data?.role != "Admin") return handleUnauthorized();
+
+        return setLoading(false);
+      }
+
+      handleUnauthorized();
+    };
+
+    getMe();
+  }, []);
+
+  return loading;
+};
