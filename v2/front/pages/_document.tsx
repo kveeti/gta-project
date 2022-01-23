7d58dd8130a8313ab46550ff31a4466c05549d4c
Veeti K <veetik@protonmail.com>
2022-01-24 00:25:29 +0200

    make mobile toolbar color match with menubar color

--- v2/front/pages/_document.tsx
+++ v2/front/pages/_document.tsx
@@ -6,11 +6,11 @@ const Doc = () => {
     <Html lang="en">
       <Head>
         <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
-
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <meta name="theme-color" content="#212121" /> {/* for mobile toolbar color */}
       </Head>
       <body>
         <Main />
