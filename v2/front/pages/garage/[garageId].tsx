932965602015878cb78a85a5a4df52e178e063b7
Veeti K <veetik@protonmail.com>
2022-01-29 03:55:05 +0200

    fixed garage page

--- v2/front/pages/garage/[garageId].tsx
+++ v2/front/pages/garage/[garageId].tsx
@@ -1,14 +1,15 @@
-import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
 import { CarGrid } from "../../components/Cards/Cars/CarGrids";
 import { GaragePageCard } from "../../components/Cards/GaragePage/GaragePageCard";
 import Layout from "../../components/Layout";
 import { Title } from "../../components/Styles/Text";
-import { apiBaseUrl, refreshCookieName } from "../../envs";
 import { ICar } from "../../interfaces/Car";
-import { IGarageDeep } from "../../interfaces/Garage";
 import { actions } from "../../state/actions";
 import { styled } from "../../stitches.config";
+import { request } from "../../util/axios";
 
 const Div = styled("div", {
   display: "flex",
@@ -18,45 +19,42 @@ const Div = styled("div", {
   paddingBottom: "0.5rem",
 });
 
-const GaragePage = (props) => {
-  const dispatch = useDispatch();
+const GaragePage = () => {
+  if (typeof window == "undefined") return null;
 
-  const garage = props.garage as IGarageDeep;
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const { garageId } = router.query;
+
+  const [garage, setGarage] = useState(null);
+
+  useEffect(() => {
+    const getGarage = async () => {
+      const res = await request(`/garages/${garageId}`, "GET");
+
+      if (res) return setGarage(res.data);
+    };
+
+    getGarage();
+  }, []);
 
   const onCarClick = (car: ICar) => {
     dispatch(actions.checked.checkCar(car));
   };
 
   return (
-    <Layout title={garage.name}>
-      <Div>
-        <GaragePageCard garage={garage} />
-        <Title>{garage.cars.length !== 0 && "Cars"}</Title>
-        {garage.cars.length !== 0 && (
-          <CarGrid cars={garage?.cars} onClick={(car) => onCarClick(car)} />
-        )}
-      </Div>
+    <Layout title={garage?.name}>
+      {garage && (
+        <Div>
+          <GaragePageCard garage={garage} />
+          <Title>{garage?.cars?.length !== 0 && "Cars"}</Title>
+          {garage?.cars?.length !== 0 && (
+            <CarGrid cars={garage?.cars} onClick={(car) => onCarClick(car)} />
+          )}
+        </Div>
+      )}
     </Layout>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-  const { garageId } = params;
-  const cookie = req.cookies[refreshCookieName];
-
-  const res = await fetch(`${apiBaseUrl}/garages/${garageId}`, {
-    headers: {
-      cookie: `${refreshCookieName}=${cookie}`,
-    },
-  });
-
-  const data = await res.json();
-
-  return {
-    props: {
-      garage: data,
-    },
-  };
 };
 
 export default GaragePage;
