0b98c2ee2a255283198c938351434b4b7abb8c19
Veeti K <veetik@protonmail.com>
2022-01-29 22:29:02 +0200

    forgot to use the global var here

--- v2/csharp-back/Backend.Api/Helpers/Cookie.cs
+++ v2/csharp-back/Backend.Api/Helpers/Cookie.cs
@@ -1,14 +1,15 @@
+using Backend.Api.Configs;
 namespace Backend.Api.Helpers;
 
 public static class Cookie
 {
   public static string CreateCookie(string aCookieValue)
   {
-    return $"refresh-token={aCookieValue}; SameSite=Strict; Secure; HttpOnly; Path=/api/auth/tokens; Max-Age={604800};";
+    return $"{CookieConfig.RefreshTokenCookie}={aCookieValue}; SameSite=Strict; Secure; HttpOnly; Path=/api/auth/tokens; Max-Age={604800};";
   }
 
   public static string GetDeleteCookie()
   {
-    return $"refresh-token=; SameSite=Strict; Secure; HttpOnly; Path=/api/auth/tokens; Max-Age=0;";
+    return $"{CookieConfig.RefreshTokenCookie}=; SameSite=Strict; Secure; HttpOnly; Path=/api/auth/tokens; Max-Age=0;";
   }
 }
