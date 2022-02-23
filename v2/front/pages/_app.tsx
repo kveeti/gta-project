f1b474176122a46e8067651a83c85f0b96371819
Veeti K <veetik@protonmail.com>
2022-02-23 17:06:56 +0200

    https://nextjs.org/docs/messages/no-document-viewport-meta

--- v2/front/pages/_app.tsx
+++ v2/front/pages/_app.tsx
@@ -1,15 +1,23 @@
 import { Provider } from "react-redux";
 import { store } from "../state/store";
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.min.css";
 import "../util/axios";
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Provider store={store}>
-    <Component {...pageProps} />
-  </Provider>
+  <>
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  </>
 );
 
 export default App;
--- v2/front/pages/_document.tsx
+++ v2/front/pages/_document.tsx
@@ -4,9 +4,6 @@ import { getCssText } from "../stitches.config";
 const Doc = () => (
   <Html lang="en">
     <Head>
-      <meta charSet="UTF-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       <link
         href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap"
